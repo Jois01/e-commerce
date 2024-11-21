6035a59f8cf807a0fc92cf0e71ce6389a90db052
Jois <putujois0703@gmail.com>
2024-11-21 16:32:10 +0800

    perbaikan foto untuk keranjang

--- src/components/cart.vue
+++ src/components/cart.vue
@@ -4,7 +4,7 @@
       <div class="card bg-white m-6 p-2 rounded-md shadow-xl">
         <div class="card-title m-4 text-2xl">My Cart</div>
         <div class="card-body m-4 items-center">
-          <div class="card bg-gray-50 rounded-lg">
+          <div class="card bg-gray-50 rounded-lg p-2">
             <div v-if="cart.length" class="container mx-auto">
               <div
                 v-for="(item, index) in cart"
@@ -12,9 +12,9 @@
                 class="flex justify-between items-center mb-4 m-8"
               >
                 <img
-                  :src="item.thumbnail || item.images || 'default.jpg'"
+                  :src="item.thumbnail || item.images || 'default.png'"
                   alt="Product Image"
-                  class="w-20 h-20 object-cover"
+                  class="w-20 h-20 object-cover rounded-md"
                 />
                 <h2 class="text-lg font-medium">{{ item.title }}</h2>
                 <div class="flex items-center">
@@ -27,13 +27,25 @@
                   </button>
                 </div>
                 <span class="text-lg font-medium">${{ item.price }}</span>
-                <button @click="removeFromCart(index)" class="text-red-500">Remove</button>
-              </div>
-              <div class="text-right mt-6">
-                <h3 class="text-xl font-bold">Total:</h3>
+                <button @click="removeFromCart(index)" class="text-red-500">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                    />
+                  </svg>
+                </button>
               </div>
             </div>
-            <div v-else class="text-center py-10">
+            <div v-else class="text-center text-xl text-gray-300 py-10">
               <p>Your cart is empty.</p>
             </div>
           </div>
@@ -47,9 +59,7 @@
         <div class="mt-4">
           <div class="flex justify-between">
             <h5 class="text-lg font-bold">Items Subtotal</h5>
-            <div class="total text-lg font-bold">
-              ${{ cart.reduce((total, item) => total + item.price * item.quantity, 0) }}
-            </div>
+            <div class="total text-lg font-bold">$ {{ cartTotal }}</div>
           </div>
           <div class="flex justify-between">
             <div>Promo</div>
@@ -57,9 +67,7 @@
           </div>
           <div class="flex justify-between">
             <h5 class="text-lg font-bold">Order Total</h5>
-            <div class="total text-lg font-bold">
-              ${{ cart.reduce((total, item) => total + item.price * item.quantity, 0) }}
-            </div>
+            <div class="total text-lg font-bold">$ {{ cartTotal }}</div>
           </div>
           <div>have a promo code?</div>
           <input type="text" class="text-center p-2" placeholder="XXX-XXX-XXX" />
@@ -87,6 +95,11 @@ export default {
     if (storedCart) {
       this.cart = storedCart
     }
+  },
+  computed: {
+    cartTotal() {
+      return this.cart.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2)
+    },
   },
   methods: {
     buttonKurang(index) {
--- src/views/DetailProductView.vue
+++ src/views/DetailProductView.vue
@@ -4,7 +4,7 @@
   >
     <div class="grid grid-cols-1 md:grid-cols-2 items-center gap-4">
       <div class="col-sel-2">
-        <img :src="product.images" alt="image" class="w-96"/>
+        <img :src="product.images" alt="image" class="w-96" />
       </div>
       <div class="items-center justify-center m-10">
         <h1 class="text-3xl font-bold text-left">{{ product.title }}</h1>
@@ -48,6 +48,7 @@ export default {
       if (isLoggedIn.value) {
         const cartItem = {
           id: this.product.id,
+          images: this.product.images,
           title: this.product.title,
           price: this.product.price,
           quantity: 1,
